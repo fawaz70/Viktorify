e490bb96a8fb251848497f473bf6ac408b3d2e6b
fawaz70 <fawaz_pirzada@hotmail.com>
2020-08-08 18:41:51 -0400

    Made some changes

--- main.py
+++ main.py
@@ -19,3 +19,46 @@ if __name__ == '__main__':
     # Use my_ranked_stats[1] for flex rank
     for i in my_ranked_stats[0]:
         print(str(i) + ': ' + str(my_ranked_stats[0][i]))
+
+    my_matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
+
+    # fetch last match detail
+    last_match = my_matches['matches'][0]
+    match_detail = watcher.match.by_id(my_region, last_match['gameId'])
+
+    participants = []
+    for row in match_detail['participants']:
+        participants_row = {}
+        participants_row['champion'] = row['championId']
+        participants_row['spell1'] = row['spell1Id']
+        participants_row['spell2'] = row['spell2Id']
+        participants_row['win'] = row['stats']['win']
+        participants_row['kills'] = row['stats']['kills']
+        participants_row['deaths'] = row['stats']['deaths']
+        participants_row['assists'] = row['stats']['assists']
+        participants_row['totalDamageDealt'] = row['stats']['totalDamageDealt']
+        participants_row['goldEarned'] = row['stats']['goldEarned']
+        participants_row['champLevel'] = row['stats']['champLevel']
+        participants_row['totalMinionsKilled'] = row['stats']['totalMinionsKilled']
+        participants_row['item0'] = row['stats']['item0']
+        participants_row['item1'] = row['stats']['item1']
+        participants.append(participants_row)
+    df = pd.DataFrame(participants)
+
+    # check league's latest version
+    latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
+    # Lets get some champions static information
+    static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
+
+    # champ static list data to dict for looking up
+    champ_dict = {}
+    for key in static_champ_list['data']:
+        row = static_champ_list['data'][key]
+        champ_dict[row['key']] = row['id']
+    for row in participants:
+        print(str(row['champion']) + ' ' + champ_dict[str(row['champion'])])
+        row['championName'] = champ_dict[str(row['champion'])]
+
+    # print dataframe
+    df = pd.DataFrame(participants)
+    df
